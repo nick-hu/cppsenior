71fa5e8a87c98e241161bc564645e7a46fc77afd
nick-hu <nicholas.squishy@gmail.com>
2014-10-16 23:11:36 -0700

    functions: quadratic

--- functions/quadratic.cpp
+++ functions/quadratic.cpp
@@ -3,17 +3,15 @@
 
 using namespace std;
 
-void quadratic(double, double, double, double &, double &, double &);
+double quadratic(double, double, double, double &, double &);
 
 int main()
 {
     double a, b, c;
     double x1, x2, d;
 
-    cin >> a;
-    cin >> b;
-    cin >> c;
-    quadratic(a, b, c, x1, x2, d);
+    cin >> a >> b >> c;
+    d = quadratic(a, b, c, x1, x2);
 
     if (d > 0) {
         cout << "x = " << x1 << ", " << x2 << endl;
@@ -28,12 +26,14 @@ int main()
     return 0;
 }
 
-void quadratic(double a, double b, double c, double &x1, double &x2, double &d)
+double quadratic(double a, double b, double c, double &x1, double &x2)
 {
-    d = pow(b, 2) - 4*a*c;
+    double d = pow(b, 2) - 4*a*c;
 
     if (d >= 0) {
         x1 = (-b + sqrt(d))/(2*a);
         x2 = (-b - sqrt(d))/(2*a);
     }
+
+    return d;
 }
