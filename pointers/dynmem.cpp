820c2445d3f0b1f8c71cd86a6102af287d42cb63
nick-hu <nicholas.squishy@gmail.com>
2015-03-02 23:04:20 -0800

    cstrings: notes WIP

--- pointers/dynmem.cpp
+++ pointers/dynmem.cpp
@@ -6,17 +6,27 @@ using namespace std;
 
 int main()
 {
-    int* n = new int;   // Allocate memory for an int
-                        // int is on the HEAP (can only be referenced through n
-                        // If n goes out of scope, int can no longer be referenced!
+    int* n = new int;
+    
+    // Allocate memory for an int
+    // int is on the HEAP (can only be referenced through n)
+    // If n goes out of scope, int can no longer be referenced!
+
     *n = 5;
     cout << *n << endl;
-    delete n;   // Frees memory for OS
-                // If n is not deleted, allocated memory will not be freed --
-                // this is a MEMORY LEAK!
-    n = 0;      // n is a NULL POINTER -- dereferencing n is now a segfault
-                // We don't want this to work: cout << *n << endl;
 
+    delete n;
+
+    // Memory is freed for OS
+    // If n is not deleted, allocated memory will not be freed -- 
+    // this is a MEMORY LEAK!
+
+    n = 0;
+
+    // n is set to the NULL POINTER -- dereferencing n is now a segfault
+    // We don't want this to work: 'cout << *n << endl;'
+
+    /*
     char ca[32];                                    // Allocate memory for (32) char array
     const char* s = "abcdefghijklmnopqrstuvwxyz123456";
     cout << "sizeof(ca): " << sizeof(ca) << endl;   // Array is 32 bytes; each char is 1 byte
@@ -29,6 +39,7 @@ int main()
     printf("%s\n", cs);                             // Print with formatting
     
     delete[] cs;
+    */
 
     return 0;
 }
