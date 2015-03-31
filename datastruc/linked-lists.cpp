8976b3e7573e9d66b46b214f0f919280c778bf03
nick-hu <nicholas.squishy@gmail.com>
2015-03-31 11:49:07 -0700

    linked lists

--- datastruc/linked-lists.cpp
+++ datastruc/linked-lists.cpp
@@ -3,21 +3,44 @@
 using namespace std;
 
 struct Node {
-    Node(unsigned int, char, Node*);
+    Node();
+    Node(char, Node*);
 
-    unsigned int num;
     char let;
     Node* next; // LEGAL to declare pointer (Node x is illegal)
 };
 
-void transverse(Node* node) {
+Node::Node() {}
+
+Node::Node(char c, Node* np=0) {
+    let = c;
+    next = np;
+}
+
+void traverse(Node* node) {
     cout << node->let << " "; // "node->let" is equivalent to "(*node).let"
     if (node->next != 0) {
-        transverse(node->next);
+        traverse(node->next);
     }
+}
+
+void PushNode(Node* &node, char c) {
+    if (node == 0) {
+        node = new Node(c);
+        return;
+    }
+    PushNode(node->next, c);
+}
+
+void PopNode(Node* &node) {
 }
 
 int main()
 {
+    Node* root = 0;
+    PushNode(root, 'b');
+    PushNode(root, 'c');
+    traverse(root);
+
     return 0;
 }
